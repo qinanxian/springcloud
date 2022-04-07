8895dfb7fdeea9a2409c6ac5735fa81d3f9e4251
hli <963586363@qq.com>
2022-04-07 23:30:01 +0800

    redis 简单集成学习

--- user-server/src/test/java/com/lih/userserver/UserServerApplicationTests.java
+++ user-server/src/test/java/com/lih/userserver/UserServerApplicationTests.java
@@ -6,6 +6,7 @@ import com.lih.userserver.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.xml.ws.Action;
 import java.util.List;
@@ -18,12 +19,14 @@ class UserServerApplicationTests {
     @Autowired
     UserController userController;
 
+    @Autowired
+    RedisTemplate <String,String> redisTemplate;
+
     @Test
     public void test(){
-        List<User> user = userMapper.getUser();
-        for (User user1 : user) {
-            System.out.println(user1);
-        }
+        redisTemplate.opsForValue().set("1","例会");
+        String s = redisTemplate.opsForValue().get("1");
+        System.out.println(s);
     }
 
 }
